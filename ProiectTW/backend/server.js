7122af9e83a3a1f3304fdef62322bf072db58b34
Harabagiu Emil-Gabriel <68844965+harabagiuemilgabriel@users.noreply.github.com>
2022-06-19 17:52:12 +0300

    d

--- ProiectTW/backend/server.js
+++ ProiectTW/backend/server.js
@@ -16,4 +16,4 @@ mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true} , () => {
 
 const PORT = process.env.PORT || 8080
 
-server.listen(PORT, "0.0.0.0", ()=> console.log(`Server running on port ${PORT}`))
+server.listen(PORT, "ec2-44-203-76-193.compute-1.amazonaws.com", ()=> console.log(`Server running on port ${PORT}`))
