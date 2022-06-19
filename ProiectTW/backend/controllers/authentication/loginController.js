bd73748b16e2ba868a39d77f923017a51d90c191
Harabagiu Emil-Gabriel <68844965+harabagiuemilgabriel@users.noreply.github.com>
2022-06-19 17:32:24 +0300

    backend

---
+++ ProiectTW/backend/controllers/authentication/loginController.js
@@ -0,0 +1,25 @@
+const requestHelper = require("../../helpers/requestHelper");
+const UserPost = require("../../models/databaseModels/UserPost");
+const LoginUserHelper = require("../../helpers/loginHelper/loginUserHelper");
+
+const loginUser = async function loginUser(req,res)
+{
+    try {
+        let body = '';
+        req.on('data', (chunk) => {
+            body += chunk.toString()
+        })
+
+        req.on('end', async ()=>{
+            let data = LoginUserHelper.loginUser(req,res, body);
+           
+        })
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = {
+    loginUser
+}
---
+++ ProiectTW/backend/router.js
@@ -0,0 +1,30 @@
+const LoginController = require("./controllers/authentication/loginController");
+const RegisterController = require("./controllers/authentication/registerController");
+
+const handleRoute = async function(req,res)
+{
+    if(req.method === 'GET')
+    {
+        if(req.url === '/api/login')
+        {
+            await LoginController.loginUser(req,res);
+        }
+    }
+    else if(req.method === 'POST')
+    {
+        if(req.url === '/api/register')
+        {
+            await RegisterController.registerUser(req,res);
+        }
+        else if(req.url === '/api/products')
+        {
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify({message: 'This is post request'}));
+        }
+    }
+}
+
+
+module.exports = {
+    handleRoute
+}
